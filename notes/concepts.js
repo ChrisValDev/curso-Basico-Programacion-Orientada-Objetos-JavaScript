b088ed69055a3537b92194969155224d51233140
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-09 17:04:49 -0600

    Que es un objeto en JavaScript

---
+++ notes/concepts.js
@@ -0,0 +1,9 @@
+// Programacion orientada a objetos (POO)
+
+// Paradigmas (estructurado, orientado a objetos y funcional) dependiendo el caso de aplicacion uno es mejor que otro o incluso al mismo tiempo.
+
+//Objetos: Los objetos literales no son iguales a los objetos de la POO (prototipo(instancia - objeto). Los objetos literales los creamos manualmente uno por uno.
+
+//Prototipos (moldes): Sirven para crear varios objetos con propiedades y metodos iguales sin necesidad de crearlos manualmente uno por uno.
+
+//Cuando se crea un objeto literal, se puede ver en consola sus atributos y uno de ellos es  __proto__: encontraremos los metodos que podremos aplicar en el objeto. (Pasa lo mismo cuando se crean los arrays, tienen su propio __proto__:)
