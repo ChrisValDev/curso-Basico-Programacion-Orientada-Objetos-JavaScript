140d3b930bf1c84d34df4a483d32270f8c1fd5a9
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-09 18:16:29 -0600

    Clases en JavaScript

--- notes/concepts.js
+++ notes/concepts.js
@@ -2,8 +2,10 @@
 
 // Paradigmas (estructurado, orientado a objetos y funcional) dependiendo el caso de aplicacion uno es mejor que otro o incluso al mismo tiempo.
 
-//Objetos: Los objetos literales no son iguales a los objetos de la POO (prototipo(instancia - objeto). Los objetos literales los creamos manualmente uno por uno.
+// Objetos: Los objetos literales no son iguales a los objetos de la POO (prototipo(instancia - objeto). Los objetos literales los creamos manualmente uno por uno.
 
-//Prototipos (moldes): Sirven para crear varios objetos con propiedades y metodos iguales sin necesidad de crearlos manualmente uno por uno.
+// Prototipos (moldes): Sirven para crear varios objetos con propiedades y metodos iguales sin necesidad de crearlos manualmente uno por uno.
 
-//Cuando se crea un objeto literal, se puede ver en consola sus atributos y uno de ellos es  __proto__: encontraremos los metodos que podremos aplicar en el objeto. (Pasa lo mismo cuando se crean los arrays, tienen su propio __proto__:)
+// Cuando se crea un objeto literal, se puede ver en consola sus atributos y uno de ellos es  __proto__: encontraremos los metodos que podremos aplicar en el objeto. (Pasa lo mismo cuando se crean los arrays, tienen su propio __proto__:)
+
+// Clases: Es la manera de crear prototipos con una sintaxis especial, existe el metodo --constructor-- el cual define los parametros, la ventaja es que al crear la instancia del objeto, permite ingresar los argumentos que solo contenga el objeto y no es obligatorio ingresar todos los parametros ni tampoco el orden en el cual estan.
