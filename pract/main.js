85b046d72a24103697466d1dd17c2cd707166afe
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-11 14:33:50 -0600

    Accediendo a propiedades de un objeto literal

---
+++ pract/main.js
@@ -0,0 +1,10 @@
+const natalia = {
+    name: "Natalia",
+    edad: 20,
+    cursosAprobados: [
+        "Curso Definitivo de HTML y CSS",
+        "Curso Practico de HTML y CSS",
+    ],
+};
+//Hacer que Natalia apruebe otro curso
+natalia.cursosAprobados.push("Curso de Responsive Design");
