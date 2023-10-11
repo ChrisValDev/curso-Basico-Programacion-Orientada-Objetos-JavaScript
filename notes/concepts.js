4b59d4aa273b4d3ff12b2e7ff82b5523880ada2c
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-11 10:38:27 -0600

    Concepto de abstraccion en JavaScript

--- notes/concepts.js
+++ notes/concepts.js
@@ -9,3 +9,5 @@
 // Cuando se crea un objeto literal, se puede ver en consola sus atributos y uno de ellos es  __proto__: encontraremos los metodos que podremos aplicar en el objeto. (Pasa lo mismo cuando se crean los arrays, tienen su propio __proto__:)
 
 // Clases: Es la manera de crear prototipos con una sintaxis especial, existe el metodo --constructor-- el cual define los parametros, la ventaja es que al crear la instancia del objeto, permite ingresar los argumentos que solo contenga el objeto y no es obligatorio ingresar todos los parametros ni tampoco el orden en el cual estan.
+
+// Abstraccion: La abstracción en JavaScript, al igual que en otros lenguajes de programación, se refiere a la capacidad de ocultar los detalles internos de un objeto o una función y exponer solo los aspectos relevantes y necesarios para interactuar con ese objeto o función. Esto ayuda a simplificar la complejidad y a mejorar la claridad del código. Ayuda tambien a reutilizar el codigo.
