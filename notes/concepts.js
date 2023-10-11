6efd38733c7d1bdee6b98dd53289a6a4c619f97d
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-11 10:46:04 -0600

    Concepto de encapsulamiento en JavaScript

--- notes/concepts.js
+++ notes/concepts.js
@@ -10,4 +10,8 @@
 
 // Clases: Es la manera de crear prototipos con una sintaxis especial, existe el metodo --constructor-- el cual define los parametros, la ventaja es que al crear la instancia del objeto, permite ingresar los argumentos que solo contenga el objeto y no es obligatorio ingresar todos los parametros ni tampoco el orden en el cual estan.
 
-// Abstraccion: La abstracción en JavaScript, al igual que en otros lenguajes de programación, se refiere a la capacidad de ocultar los detalles internos de un objeto o una función y exponer solo los aspectos relevantes y necesarios para interactuar con ese objeto o función. Esto ayuda a simplificar la complejidad y a mejorar la claridad del código. Ayuda tambien a reutilizar el codigo.
+// 4 pilares de la POO
+
+// 1. Abstraccion: La abstracción en JavaScript, al igual que en otros lenguajes de programación, se refiere a la capacidad de ocultar los detalles internos de un objeto o una función y exponer solo los aspectos relevantes y necesarios para interactuar con ese objeto o función. Esto ayuda a simplificar la complejidad y a mejorar la claridad del código. Ayuda tambien a reutilizar el codigo.
+
+// 2. Encapsulamiento: El encapsulamiento en JavaScript se refiere a la práctica de ocultar detalles internos y la implementación de un objeto o una función, y solo exponer una interfaz pública que permita interactuar con dicho objeto o función. Esto se logra utilizando propiedades y métodos para controlar el acceso a los datos y las funcionalidades internas del objeto. El encapsulamiento es uno de los cuatro conceptos fundamentales de la programación orientada a objetos, junto con la abstracción, la herencia y el polimorfismo.
